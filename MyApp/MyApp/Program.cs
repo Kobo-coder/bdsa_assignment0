32b04d26938fe21a4d579cc4708c3a4219c3e9f0
Kobo <73228675+Kobo-coder@users.noreply.github.com>
2021-09-08 08:28:41 +0200

    Add leap year function + test (#1)

--- MyApp/MyApp/Program.cs
+++ MyApp/MyApp/Program.cs
@@ -2,11 +2,29 @@
 
 namespace MyApp
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter a year, to check if it is a leap year. \n Then press [Enter]");
+            int year = int.Parse(Console.ReadLine());
+            IsLeapYear(year);
+            //Console.WriteLine("Hello World!");
+        }
+        public static bool IsLeapYear(int year){
+            if(year%4 == 0){
+                if(year%400 == 0){
+                    Console.WriteLine("yay");
+                    return true;
+                } else if(year%100 == 0){
+                    Console.WriteLine("nay");
+                    return false;
+                }
+                Console.WriteLine("yay");
+                return true;
+            }
+            Console.WriteLine("nay");
+            return false;
         }
     }
 }
