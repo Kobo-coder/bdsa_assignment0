ab5ec6fdaf308490816458444664edca47c74d72
Kobo-coder <jakob.hjalgrim@gmail.com>
2021-09-08 12:50:48 +0200

    Add Error Handling + Tests

--- MyApp/MyApp.Tests/UnitTest1.cs
+++ MyApp/MyApp.Tests/UnitTest1.cs
@@ -1,5 +1,6 @@
 using System;
 using Xunit;
+using System.IO;
 
 namespace MyApp.Tests
 {
@@ -11,11 +12,23 @@ namespace MyApp.Tests
             Assert.True(Program.IsLeapYear(1600));
             Assert.True(Program.IsLeapYear(1604));
             Assert.False(Program.IsLeapYear(1700));
-            Assert.True(Program.IsLeapYear(4));
+            Assert.False(Program.IsLeapYear(1802));
         }
 
-        public void Test2(){
-            
-        }
+        [Fact]
+        public void Test2() {
+            Assert.Throws<ArgumentException>(() => Program.IsLeapYear(1581));
+        }  
+
+        [Fact]
+        public void Test3() {
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+            Program.Main(new string[0]);
+            var outputarray = writer.ToString().Split("\n");
+            //Having difficulty fixing the fact that there is a need to press enter to complete this test.
+            Assert.Equal("Input string was not in a correct format.", outputarray[3]);
+        }   
+
     }
 }
--- MyApp/MyApp/Program.cs
+++ MyApp/MyApp/Program.cs
@@ -4,14 +4,23 @@ namespace MyApp
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("Please enter a year, to check if it is a leap year. \n Then press [Enter]");
-            int year = int.Parse(Console.ReadLine());
-            IsLeapYear(year);
-            //Console.WriteLine("Hello World!");
+            try{
+                int year = int.Parse(Console.ReadLine());
+                IsLeapYear(year);
+            } catch (ArgumentException e) {
+                Console.WriteLine(e.Message);
+            } catch (FormatException e) {
+                Console.WriteLine(e.Message);
+            }
         }
+
         public static bool IsLeapYear(int year){
+            if(year < 1582){
+                throw new ArgumentException("The year must be 1582 or above");
+            }
             if(year%4 == 0){
                 if(year%400 == 0){
                     Console.WriteLine("yay");
